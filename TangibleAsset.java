37264c4fa1786e1d02dcd8589dc8be9f637e0bb9
shinjyuku2024 <163490440+shinjyuku2024@users.noreply.github.com>
2024-03-16 12:55:54 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,22 @@
+public abstract class TangibleAsset{
+  private String name;
+  private int price;
+  private String color;
+
+ public TangibleAsset(String nmae, int price, String color) {
+   this.name = name;
+   this.price = price; 
+   this.color = color;
+ }
+
+public String getName() { 
+        return this.name;
+}
+public int grtPrice() {
+        return this.price;
+}
+public String getColor() {
+        return this.color;
+}
+}
+        
